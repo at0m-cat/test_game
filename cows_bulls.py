3a3a44df4170db1e59e0edd5ac9f7f4097687fec
at0m-cat <at0mc@proton.me>
2023-02-23 01:29:42 +0700

    Нужно сделать множество проверок. Вылетает если string совпадает с размером number. Добавлен счетчик, предложение начать игру заново.

--- cows_bulls.py
+++ cows_bulls.py
@@ -18,9 +18,18 @@ while True:
         print('Проверяем количество быков и коров ...')
         SetNumber(number)
         print(mastermind_engine._animals)
-        # print('ЧИСЛО ОТ ИИ ', GetNumber())
+        print('ЧИСЛО ОТ ИИ ', GetNumber())
         if mastermind_engine.BULL == 4:
-            print('МОЛОДЕЦ, МАЛОЙ. Победил за {} ходов'
+            print('МОЛОДЕЦ, МАЛОЙ! Победил за {} ходов'
                   .format(mastermind_engine._counter))
-            break
+            print('Еще партию? Y/N:')
+            _ = input()
+            if _ == 'Y':
+                GenerateNumber()
+                mastermind_engine._counter = 0
+                continue
+            if _ == 'N':
+                break
+            else:
+                pass
     continue
